e8d20918e4b3e37f3dad0c7d0f39aa167a794897
MouraYuri <mourayuri339@gmail.com>
2019-11-09 01:27:07 -0300

    pandas added

--- KMeans.py
+++ KMeans.py
@@ -8,38 +8,32 @@ def preProcessingData(data):
     for x in range(len(data)):
         data[x] = data[x].split(' ')
         data[x][1] = data[x][1].rstrip()
-    return data
+        data[x][0], data[x][1] = int(data[x][0]), int(data[x][1])
+    df = pd.DataFrame(data, columns = ['xCordinate', 'yCordinate'])
+    return df
 
 def kMeans(dataset,  k):
-    centroidXcoordinate, centroidYcoordinate = [], []
-    setXpoints,setYpoints = [], []
-    for x in dataset:
-        setXpoints.append(int(x[0]))
-        setYpoints.append(int(x[1]))
+    centroidDataframe = pd.DataFrame([], columns = ['xCordinate', 'yCordinate'])
     for y in range(k):
-        centroidXcoordinate.append(randint(numpy.amin(setXpoints),numpy.amax(setXpoints)))
-        centroidYcoordinate.append(randint(numpy.amin(setYpoints),numpy.amax(setYpoints)))
+        centroidDataframe.insert(column="xCordinate", value=(randint(dataset['xCordinate'].min(), dataset['xCordinate'].max())))
+        centroidDataframe.insert(column="yCordinate", value=(randint(dataset['yCordinate'].min(), dataset['yCordinate'].max())))
+    print(centroidDataframe)
 
 
-
-
+'''
     plt.scatter(centroidXcoordinate, centroidYcoordinate, color=['blue'])
     plt.scatter(setXpoints, setYpoints, color=['red'])
-    '''    
+
     for x in range(len(pontosX)):
         for y in range(k):
-    '''
+
 
 
     plt.show()
-
+'''
 
 data = open("C:/Users/YuriPc/Desktop/Python Projects/KMeans/data.txt", 'r')
 data = data.readlines()
 data = preProcessingData(data)
 print(data)
-
-
-
-
 kMeans(data, 2)
